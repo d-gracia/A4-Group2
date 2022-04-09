bce0c3c4e6e1b9db67cf6f37a692e1bfa0d78d5b
asw51 <asw55551111@gmail.com>
2022-04-09 11:27:09 -0400

    put weather api in flask

--- backend/app.py
+++ backend/app.py
@@ -19,10 +19,29 @@ app = Flask(__name__)
 
 @app.route("/", methods=['GET'])
 def index():
+    # Simbad API
     req = requests.get('http://simbad.u-strasbg.fr/simbad/sim-coo?output.format=ASCII&Coord=12%2030%20%2b10%2020&Radius=5&Radius.unit=arcmin')
-    data = req.content
-    print(data)
-    return render_template('index.html',data=data)
+    simbad = req.content
+    print(simbad)
+
+    # Weather API
+    lat="42.360081"
+    lon="-71.058884"
+    weather_exclude = "minutely,hourly,daily,alerts" 
+    """For a location it can output: current,minutely,hourly,daily,alerts
+
+    We likely do not need all of this, so either we can write a simple switch
+    to figure out which outputs we want, or we can just get all of it and 
+    only return what the front end asks for."""
+
+    weather_api_url = "https://api.openweathermap.org/data/2.5/onecall?lat="+lat+"&lon="+lon+"&exclude="+weather_exclude+"&appid=0f4cf3136e2801a4208f7b57cada4f2b"
+    weather = requests.get(weather_api_url)
+
+    #print(weather.json())  #prints out entire responce
+    weatherd= weather.json()["current"]["weather"][0]["description"]
+    print(weather.json()["current"]["weather"][0]["description"])
+
+    return render_template('index.html', data1=simbad, data2=weatherd)
 
 
 
