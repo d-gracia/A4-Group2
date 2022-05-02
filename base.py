0f4de271a964d21adcaf577f457eff2dde053164
asw51 <79736690+asw51@users.noreply.github.com>
2022-05-02 17:35:18 -0400

    recs as separate below

--- base.py
+++ base.py
@@ -1,9 +1,10 @@
 from flask import Flask, render_template
-from flask import request
+from flask import request, session
 import requests
 import json
 from flask import Flask, request, jsonify, current_app
 from flask_cors import CORS
+from flask_session import Session
 
 app = Flask(__name__)
 
@@ -28,16 +29,19 @@ def testPost():
     weather_exclude = "minutely,daily,alerts" 
     weather_api_url = "https://api.openweathermap.org/data/2.5/onecall?lat="+lat+"&lon="+lon+"&exclude="+weather_exclude+"&appid=0f4cf3136e2801a4208f7b57cada4f2b"
     weather = requests.get(weather_api_url)
+  
     i = 0
     weatherd = ""
     while i < 48:
-        weatherd = weatherd + "Weather in " + str(i) + " hour(s): " + weather.json()["hourly"][i]["weather"][0]["description"] + ".  "
-        i += 1
-    # weatherd= weather.json()["hourly"][5]["weather"][0]["description"] + " " + weather.json()["hourly"][0]["weather"][0]["description"]
+       weatherd = weatherd + "Weather in " + str(i) + " hour(s): " + weather.json()["hourly"][i]["weather"][0]["description"] + ".  "
+       i += 1
+
+    #weatherd = weather.json()["hourly"][5]["weather"][0]["description"] + " " + weather.json()["hourly"][0]["weather"][0]["description"]
     print("\n" + "Current Weather: " + weather.json()["current"]["weather"][0]["description"] + "\n")
 
-    return jsonify(name=weatherd)
+    return jsonify(name=weatherd )
 
+    
 @app.post('/post2')
 def testPost2():
     name2 = request.json.get('name2')
@@ -69,7 +73,42 @@ def testPost2():
     
     return jsonify(name2=simbad)
     
-    
+@app.post('/post3')
+def testPost3():
+    name3 = request.json.get('name3')
+    current_app.logger.debug(name3)
+    print(str(name3))
+
+    postalCode = str(name3)
+    map_url="https://dev.virtualearth.net/REST/v1/Locations/US/"+postalCode+"?&key=Ag8WDTJmVQA6MknifiagqrnEH1AaAv3ce03GeTcN2rYX7mbqzxzG31hX0MChiZlC"
+    map = requests.get(map_url)
+
+    #print(map.json())  #prints out entire responce
+    print("\n")
+    print(map.json()['resourceSets'][0]["resources"][0]['bbox'][0]) #prints out just the 'latitude' value
+    print(map.json()['resourceSets'][0]["resources"][0]['bbox'][1]) #prints out just the 'longitude' value
+
+    lat = str(map.json()['resourceSets'][0]["resources"][0]['bbox'][0])
+    lon = str(map.json()['resourceSets'][0]["resources"][0]['bbox'][1])
+
+    weather_exclude = "minutely,daily,alerts" 
+    weather_api_url = "https://api.openweathermap.org/data/2.5/onecall?lat="+lat+"&lon="+lon+"&exclude="+weather_exclude+"&appid=0f4cf3136e2801a4208f7b57cada4f2b"
+    weather = requests.get(weather_api_url)
+    #weather = session.get('weather', None)
+    go = ["clear skys" , "few clouds"] 
+    hours = [10,20,30,40]
+    recs= ''
+    for i in hours:
+            found_a_string = False
+            for item in go:    
+                if item in weather.json()["hourly"][i]["weather"][0]["description"]:
+                    found_a_string = True
+
+            if found_a_string:
+                 recs = recs + " In " + str(i) + " hours " + "may be a good time to view stars!"
+            else:
+                 recs =recs + " In " + str(i) + " hours, " +"we do not reccomend going to view stars."
+    return jsonify(name3=recs)    
 
 
 # because backend and frontend use different ports, we have to enable cross-origin requests
